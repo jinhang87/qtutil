28eb029b131cc3b7f7c3ccdea5d914e510f62e15
jinhang87 <jinhang87@163.com>
2017-05-15 22:19:35 +0800

    [chg] Worker parent parameter is not allowed;

--- epl_QObject_moveToThread/mainwindow.cpp
+++ epl_QObject_moveToThread/mainwindow.cpp
@@ -9,23 +9,41 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    worker = new Worker(this);
+    worker = new Worker();
     thread = new QThread;
+    //线程和具体任务操作绑定
     worker->moveToThread(thread);
+    //线程开始信号绑定
     connect(thread, &QThread::started, worker, &Worker::start);
+    //线程结束信号绑定
     connect(thread, &QThread::finished, worker, &QObject::deleteLater);
+    //线程控制信号绑定
     connect(this, &MainWindow::operate, worker, &Worker::dowork);
+    //线程结果上报信号绑定
     connect(worker, &Worker::resultReady, this, &MainWindow::handleResult);
+    //界面按钮信号绑定
     connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::sendopeatesignal);
+    //启动线程
     thread->start();
     qDebug() << "MainWindow threadid : " << QThread::currentThreadId();
 }
 
 MainWindow::~MainWindow()
 {
+    //线程开始信号解绑定
+    disconnect(thread, &QThread::started, worker, &Worker::start);
+    //线程结束信号解绑定
+    disconnect(thread, &QThread::finished, worker, &QObject::deleteLater);
+    //线程控制信号解绑定
+    disconnect(this, &MainWindow::operate, worker, &Worker::dowork);
+    //线程结果上报信号解绑定
+    disconnect(worker, &Worker::resultReady, this, &MainWindow::handleResult);
+    //界面按钮信号解绑定
+    disconnect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::sendopeatesignal);
     thread->quit();
     thread->wait();
     delete thread;
+    delete worker;
     delete ui;
 }
 
