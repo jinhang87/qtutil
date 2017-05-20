f292d79c47fc07f95610bbb775270373362c9a52
jinhang <jinhang87@163.com>
2017-05-20 14:33:39 +0800

    [add] app_GetSreenInfo

---
+++ app_GetSreenInfo/main.cpp
@@ -0,0 +1,54 @@
+#include "mainwindow.h"
+#include <QGuiApplication>
+#include <QScreen>
+#include <QDebug>
+
+/*
+  Example of using Qt 5 QScreen class.
+*/
+
+// Helper function to return display orientation as a string.
+QString Orientation(Qt::ScreenOrientation orientation)
+{
+    switch (orientation) {
+        case Qt::PrimaryOrientation           : return "Primary";
+        case Qt::LandscapeOrientation         : return "Landscape";
+        case Qt::PortraitOrientation          : return "Portrait";
+        case Qt::InvertedLandscapeOrientation : return "Inverted landscape";
+        case Qt::InvertedPortraitOrientation  : return "Inverted portrait";
+        default                               : return "Unknown";
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication a(argc, argv);
+
+    qDebug() << "Number of screens:" << QGuiApplication::screens().size();
+
+    qDebug() << "Primary screen:" << QGuiApplication::primaryScreen()->name();
+
+    foreach (QScreen *screen, QGuiApplication::screens()) {
+        qDebug() << "Information for screen:" << screen->name();
+        qDebug() << "  Available geometry:" << screen->availableGeometry().x() << screen->availableGeometry().y() << screen->availableGeometry().width() << "x" << screen->availableGeometry().height();
+        qDebug() << "  Available size:" << screen->availableSize().width() << "x" << screen->availableSize().height();
+        qDebug() << "  Available virtual geometry:" << screen->availableVirtualGeometry().x() << screen->availableVirtualGeometry().y() << screen->availableVirtualGeometry().width() << "x" << screen->availableVirtualGeometry().height();
+        qDebug() << "  Available virtual size:" << screen->availableVirtualSize().width() << "x" << screen->availableVirtualSize().height();
+        qDebug() << "  Depth:" << screen->depth() << "bits";
+        qDebug() << "  Geometry:" << screen->geometry().x() << screen->geometry().y() << screen->geometry().width() << "x" << screen->geometry().height();
+        qDebug() << "  Logical DPI:" << screen->logicalDotsPerInch();
+        qDebug() << "  Logical DPI X:" << screen->logicalDotsPerInchX();
+        qDebug() << "  Logical DPI Y:" << screen->logicalDotsPerInchY();
+        qDebug() << "  Orientation:" << Orientation(screen->orientation());
+        qDebug() << "  Physical DPI:" << screen->physicalDotsPerInch();
+        qDebug() << "  Physical DPI X:" << screen->physicalDotsPerInchX();
+        qDebug() << "  Physical DPI Y:" << screen->physicalDotsPerInchY();
+        qDebug() << "  Physical size:" << screen->physicalSize().width() << "x" << screen->physicalSize().height() << "mm";
+        qDebug() << "  Primary orientation:" << Orientation(screen->primaryOrientation());
+        qDebug() << "  Refresh rate:" << screen->refreshRate() << "Hz";
+        qDebug() << "  Size:" << screen->size().width() << "x" << screen->size().height();
+        qDebug() << "  Virtual geometry:" << screen->virtualGeometry().x() << screen->virtualGeometry().y() << screen->virtualGeometry().width() << "x" << screen->virtualGeometry().height();
+        qDebug() << "  Virtual size:" << screen->virtualSize().width() << "x" << screen->virtualSize().height();
+    }
+    return a.exec();
+}
