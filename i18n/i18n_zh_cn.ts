f30e82b7b996bf174fa37da2b2f2fbc9228fa313
jinhang87 <jinhang87@163.com>
2017-06-15 22:01:47 +0800

    [add] i18n.exe

---
+++ i18n/i18n_zh_cn.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>主窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>PushButton</source>
+        <translation>按钮</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>TextLabel</source>
+        <translation>标签</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="22"/>
+        <source>plugins</source>
+        <translation>插件</translation>
+    </message>
+</context>
+</TS>
--- i18n/main.cpp
+++ i18n/main.cpp
@@ -20,6 +20,8 @@ int main(int argc, char *argv[])
     }
 
     qDebug() << QObject::tr("plugins");
+    qDebug() << "ok";
+
     MainWindow w;
     w.show();
 
