9d52f18fc2a128686b2f32c3f0c8c3c3e5617b0b
jinhang <jinhang87@163.com>
2017-08-30 10:51:17 +0800

    if

--- gui/windowopacityanimation/main.cpp
+++ gui/windowopacityanimation/main.cpp
@@ -10,35 +10,38 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-//    //指定某个控件透明，windows ok
-//    Widget w;
-//    w.show();
+#define TEST 5
 
-//    //窗体透明，窗体的控件透明，windows ok
-//    //embeded linux 正常
-//    //windows 正常
-//    WidgetA w;
-//    w.show();
-
-//    //窗体透明，窗体的控件不透明，windows ok
-//    WidgetB w;
-//    w.show();
-
-//    //窗体不透明，窗体的局部区域透明，窗体上的控件不透明
-//    //embeded linux 正常
-//    //windows 局部区域变黑，控件显示不了
-//    WidgetC w;
-//    w.show();
-
-//    //窗体透明，控件不透明
-//    //embeded linux 正常
-//    //windows 背景变黑，控件显示不了
-//    WidgetD w;
-//    w.show();
-
+#if (TEST==1)
+    //指定某个控件透明，windows ok
+    Widget w;
+    w.show();
+#elif (TEST==2)
+    //窗体透明，窗体的控件透明，windows ok
+    //embeded linux 正常
+    //windows 正常
+    WidgetA w;
+    w.show();
+#elif (TEST==3)
+    //窗体透明，窗体的控件不透明，windows ok
+    WidgetB w;
+    w.show();
+#elif (TEST==4)
+    //窗体不透明，窗体的局部区域透明，窗体上的控件不透明
+    //embeded linux 正常
+    //windows 局部区域变黑，控件显示不了
+    WidgetC w;
+    w.show();
+#elif (TEST==5)
+    //窗体透明，控件不透明
+    //embeded linux 正常
+    //windows 背景变黑，控件显示不了
+    WidgetD w;
+    w.show();
+#elif (TEST==6)
     //控件透明颜色属性
     WidgetQss w;
     w.show();
-
+#endif
     return a.exec();
 }
