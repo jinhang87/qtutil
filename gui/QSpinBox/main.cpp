3eba60f14823c55e7d933a4d20e659fa63d03291
jinhang87@163.com <jinhang87@163.com>
2017-11-25 17:14:12 +0800

    qspinbox

--- gui/QSpinBox/main.cpp
+++ gui/QSpinBox/main.cpp
@@ -1,9 +1,17 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile stylefile(":/spinbox.qss");
+    stylefile.open(QFile::ReadOnly);
+    QString qssText = QString::fromLocal8Bit(stylefile.readAll());
+    a.setStyleSheet(qssText);
+    stylefile.close();
+
     MainWindow w;
     w.show();
 
