453848e1c1c46043d6a5909e22b5fc216829f16a
jinhang87@163.com <jinhang87@163.com>
2017-11-27 12:28:23 +0800

    提交编码方式

--- core/codec/gbkdialog.cpp
+++ core/codec/gbkdialog.cpp
@@ -1,15 +1,18 @@
 #include "gbkdialog.h"
 #include "ui_gbkdialog.h"
-
+#include <QDebug>
 //本文件为GBK编码，使用QtCreator配置GBK编码，然后编辑文件，用notepad++右下角查看编码方式
+
+#define QLABLE_CN u8"QLabel { border-image: url(:/剪切-click-.png);"
 
 GbkDialog::GbkDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::GbkDialog)
 {
     ui->setupUi(this);
-    setStyleSheet("QLabel { border-image: url(:/-click-.png); }"
-                  "QLabel { border-image: url(:/剪切-click-.png); }");
+    setStyleSheet(u8"QLabel#labelen { border-image: url(:/-click-.png); }"
+                  u8"QLabel#labelcn { border-image: url(:/剪切-click-.png); }");
+    qDebug() << QLABLE_CN;
 }
 
 GbkDialog::~GbkDialog()
